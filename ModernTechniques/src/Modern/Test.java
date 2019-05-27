b17cb8592ad41c89a28efbf4d1c237de3de18864
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-05-27 13:36:39 -0400

    first git hub commit

--- ModernTechniques/src/Modern/Test.java
+++ ModernTechniques/src/Modern/Test.java
@@ -6,6 +6,7 @@ public class Test {
             // TODO Auto-generated method stub
             System.out.println("HELLO Modern");
             System.out.println("ist change");
+            System.out.println("change for github");
       }
 
 }
