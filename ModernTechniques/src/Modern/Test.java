e3f9a38fd9bea24fe393cb8704355fff1a78a360
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-05-27 13:34:42 -0400

    first commit

---
+++ ModernTechniques/src/Modern/Test.java
@@ -0,0 +1,11 @@
+package Modern;
+
+public class Test {
+
+      public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            System.out.println("HELLO Modern");
+            System.out.println("ist change");
+      }
+
+}
