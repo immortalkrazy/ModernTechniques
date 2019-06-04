abf306909922be8ed9ddb2be29ddd1e7f1b5e7a7
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-06-04 00:40:13 -0400

    work on clone

---
+++ ModernTechniques/src/Clone/House.java
@@ -0,0 +1,11 @@
+package Clone;
+
+public class House implements Cloneable, Comparable<House> {
+
+      @Override
+      public int compareTo(House o) {
+            // TODO Auto-generated method stub
+            return 0;
+      }
+
+}
---
+++ ModernTechniques/src/Clone/HouseClient.java
@@ -0,0 +1,10 @@
+package Clone;
+
+public class HouseClient {
+
+      public static void main(String[] args) {
+            // TODO Auto-generated method stub
+
+      }
+
+}
