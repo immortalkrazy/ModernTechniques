7039d9521cebaeb4aa92091ef2fca80698289d99
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2021-02-24 22:09:29 -0500

    added CheckIfExist

--- ModernTechniques/src/leetCode/TestClass.java
+++ ModernTechniques/src/leetCode/TestClass.java
@@ -4,9 +4,16 @@ public class TestClass {
 
       public static void main(String[] args) {
 
+            // Test for RomanToInt
             RomanToInt ri = new RomanToInt();
 
             System.out.println(ri.romanToInt("XXVII"));
+
+            // test for CheckIfExist
+            CheckIfExist ce = new CheckIfExist();
+            int[] arr = { 2, 3, 5, 8, 12 };
+
+            System.out.println(ce.checkIfExist(arr));
       }
 
 }
