549a5c7ad845a73487cfafd26a90cc4b9674cb7c
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2021-03-17 00:55:45 -0400

    testing

--- ModernTechniques/src/Modern/Test.java
+++ ModernTechniques/src/Modern/Test.java
@@ -3,10 +3,24 @@ package Modern;
 public class Test {
 
       public static void main(String[] args) {
-            // TODO Auto-generated method stub
-            System.out.println("HELLO Modern");
-            System.out.println("ist change");
-            System.out.println("change for github");
-      }
 
+            long val = Long.MAX_VALUE + 1;
+            System.out.println(val);
+
+            try {
+                  int value = 50;
+                  if (value < 40) {
+                        throw new Exception("Value is too small");
+                  }
+            } catch (Exception e) {
+                  System.out.println(e.getMessage());
+            }
+            System.out.println("Continue after the catch block");
+
+            int[] list = new int[5];
+            System.out.println(list[4]);
+
+            String string = "abc";
+            System.out.println(string.charAt(2));
+      }
 }
