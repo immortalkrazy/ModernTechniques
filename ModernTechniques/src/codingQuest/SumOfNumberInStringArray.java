0ad2024f566f9a8de203dcaa39718f90fe8c8f6a
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2021-11-11 16:09:20 -0500

    minor changes in sumOfIntegers in string array but not complete yet

--- ModernTechniques/src/codingQuest/SumOfNumberInStringArray.java
+++ ModernTechniques/src/codingQuest/SumOfNumberInStringArray.java
@@ -4,31 +4,46 @@ public class SumOfNumberInStringArray {
 
       public static void main(String[] args) {
 
-            String[] arr = { "2AA", "B3C", "Z27", "LM6", "BAC" };
+            String arr[] = { "2AA", "B3C", "Z27", "LM6", "BAC" };
 
             System.out.println("hello");
 
-            int result = sumOfNumbers(arr);
-
-            System.out.println(result);
-
-      }
-
-      public static int sumOfNumbers(String[] strArray) {
-
             int sum = 0;
 
-            for (int i = 0; i < strArray.length; i++) {
+            for (int i = 0; i < arr.length; i++) {
 
-                  String strElement = strArray[i];
-                  for (int j = 0; j < strElement.length(); i++) {
+                  String strElement = arr[i];
+
+                  for (int j = 0; j < strElement.length() - 1; i++) {
+
                         if (Character.isDigit(strElement.charAt(j))) {
-                              sum = sum + Integer.valueOf(strElement.charAt(j));
+
+                              sum = sum + Integer.parseInt(strElement.charAt(j) + "");
                         }
                   }
             }
 
-            return sum;
+//             int result = sumOfNumbers(arr);
+
+            System.out.println(sum);
+
       }
+
+//      public static int sumOfNumbers(String[] strArray) {
+//
+//            int sum = 0;
+//
+//            for (int i = 0; i < strArray.length; i++) {
+//
+//                  String strElement = strArray[i];
+//                  for (int j = 0; j < strElement.length(); i++) {
+//                        if (Character.isDigit(strElement.charAt(j))) {
+//                              sum = sum + Integer.valueOf(strElement.charAt(j));
+//                        }
+//                  }
+//            }
+//
+//            return sum;
+//      }
 
 }
